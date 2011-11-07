36522e6e32873a6a35a11eff8af9682bcdea8474
Alexei Romanoff <drednout.by@gmail.com>
2011-11-07 19:00:24 +0000

    Initial import

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_ms_import_export do
+  name 'Redmine Ms Import Export plugin'
+  author 'Alexei Romanoff'
+  description 'Basic Import/Export from MS Project to Redmine and vice versa.'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
